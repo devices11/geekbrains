62dfe4937b7860e6b8e3eca53554c5f68ea83ec6
Valentin Izotov <devices11@gmail.com>
2019-11-20 00:08:49 +0300

    First commit in geekbrains

--- src/main/java/FirstBlock/FirstBlock.java
+++ src/main/java/FirstBlock/FirstBlock.java
@@ -1,7 +1,36 @@
 package FirstBlock;
 
+import java.util.Scanner;
+
 public class FirstBlock {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+
+        // 1.
+        byte b = 10;
+        short s = 2404;
+        int i = 1234;
+        long l = 1500L;
+        float f = 120.0f;
+        double d = 15.75;
+        boolean bool = true;
+        char c = 'F';
+        String str = "Vaska";
+
+        System.out.println(b);
+        System.out.println(s);
+        System.out.println(i);
+        System.out.println(l);
+        System.out.println(f);
+        System.out.println(d);
+        System.out.println(bool);
+        System.out.println(c);
+        System.out.println(str);
+
+        // 2.
+
+        Scanner scanner = new Scanner(System.in);
+        String op = scanner.nextLine();
+
+        System.out.println(op);
     }
 }
