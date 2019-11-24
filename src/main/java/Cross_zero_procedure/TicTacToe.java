fdee84193aad4da84ce40d29138a38d8fa38d66b
Valentin Izotov <devices11@gmail.com>
2019-11-24 15:50:39 +0300

    TicTacToe game this HardMode

--- src/main/java/Cross_zero_procedure/TicTacToe.java
+++ src/main/java/Cross_zero_procedure/TicTacToe.java
@@ -4,149 +4,175 @@ import java.util.Random;
 import java.util.Scanner;
 
 /**
- * Created by Izotov V.A. on 23.11.2019.
+ * Created by Izotov V.A. on 24.11.2019.
  */
 public class TicTacToe {
 
-    private static char[][] map; // матрица игры
-    private static int SIZE = 3; //размер поля
+    public static final String DOT_X = "X";
+    public static final String DOT_O = "O";
+    public static final String DOT_EMPTY = "•";
+    public static int aiLevel = 2;
+    public static final int SIZE = 3;
 
-    private static final char DOT_EMPTY = '•';
-    private static final char DOT_X= 'X';
-    private static final char DOT_O = 'O';
-
-    private static final boolean SILLY_MODE = true;
+    public static String[][] map = new String[SIZE][SIZE];
 
     private static Scanner scanner = new Scanner(System.in);
     private static Random random = new Random();
 
     public static void main(String[] args) {
-        initMap();
-        printMap();
-
-        while (true) {
-            humanTurn(); //ход человека
-            if (isEndGame(DOT_X)) {
+        System.out.println("Выберите сложность компьютера: ");
+        System.out.println("1. Простой.");
+        System.out.println("2. Продвинутый.");
+        System.out.println("3. Выход.");
+        int i = 0;
+        Scanner sc = new Scanner(System.in);
+        i = sc.nextInt();
+        switch (i) {
+            case 1: {
+                aiLevel = 1;
+                modeAgainstAI();
                 break;
             }
-
-            computerTurn(); //ход компьютера
-            if (isEndGame(DOT_O)) {
+            case 2: {
+                aiLevel = 2;
+                modeAgainstAI();
                 break;
             }
-
+            case 3: {
+                System.exit(0);
+                break;
+            }
+            default: {
+                System.out.println("Было введено неверное значение!");
+            }
         }
-        System.out.println("Игра окончена");
     }
 
-    private static void initMap() {
-        map = new char[SIZE][SIZE];
-        for(int i = 0; i < SIZE; i ++){
-            for(int j = 0; j < SIZE; j++){
+    public static void modeAgainstAI() {
+        int count = 0;
+        initMap();
+        while (true) {
+            printMap();
+            humanTurn(DOT_X, 1);
+            count++;
+            if (checkWin(DOT_X)) {
+                System.out.println("Победил пользователь!!!");
+                printMap();
+                break;
+            }
+            computerTurn();
+            count++;
+            if (checkWin(DOT_O)) {
+                System.out.println("Победил компьютер!!!");
+                printMap();
+                break;
+            }
+            if (count == Math.pow(SIZE, 2)) {
+                printMap();
+                break;
+            }
+        }
+    }
+
+    public static void initMap() {
+        for (int i = 0; i < SIZE; i++) {
+            for (int j = 0; j < SIZE; j++) {
                 map[i][j] = DOT_EMPTY;
             }
         }
     }
 
-    private static void printMap() {
-        for(int i = 0; i <= SIZE; i++){
+    public static void printMap() {
+        for (int i = 0; i <= SIZE; i++) {
             System.out.print(i + " ");
         }
         System.out.println();
-
-        for(int i =0; i < SIZE; i++){
-            System.out.print((i+1) + " ");
-            for(int j = 0; j < SIZE; j++){
+        for (int i = 0; i < SIZE; i++) {
+            System.out.print((i + 1) + " ");
+            for (int j = 0; j < SIZE; j++) {
                 System.out.print(map[i][j] + " ");
             }
             System.out.println();
         }
-
-        System.out.println();
     }
 
-    private static void humanTurn() {
-        int x, y;
-        do {
-            System.out.println("Введите координаты ячейки (X Y)");
-            y = scanner.nextInt() - 1; // Считывание номера строки
-            x = scanner.nextInt() - 1; // Считывание номера столбца
-        }
-        while(!isCellValid(x, y));
-
-        map[y][x] = DOT_X;
-    }
-
-    private static void computerTurn() {
+    public static void humanTurn(String sign, int i) {
         int x = -1;
         int y = -1;
+        do {
+            if (i == 0) {
+                System.out.println("Введите координаты x y (1 - " + SIZE + "): ");
+            } else {
+                System.out.println("Игрок " + i + ". Введите координаты x y (1 - " + SIZE + "): ");
+            }
+            x = scanner.nextInt() - 1;
+            y = scanner.nextInt() - 1;
+        }
+        while (isCellValid(x, y));
+        map[x][y] = sign;
+    }
 
-        if (SILLY_MODE) {
-            do {
-                x = random.nextInt(SIZE);
-                y = random.nextInt(SIZE);
-            } while (!isCellValid(x, y));
+    public static void computerTurn() {
+        int x = -1;
+        int y = -1;
+        boolean ai_win = false;
+        boolean user_win = false;
 
-            System.out.println("Компьютер выбрал ячейку " + (y + 1) + " " + (x + 1));
-            map[y][x] = DOT_O;
-        } else {
-            for(int i = 0; i < SIZE; i++){
-                for(int j = 0; j < SIZE; j++){
-                    int best_x = -1;
-                    int best_y = -1;
-
-
+        // Находим выигрышный ход
+        if (aiLevel == 2) {
+            for (int i = 0; i < SIZE; i++) {
+                for (int j = 0; j < SIZE; j++) {
+                    if (!isCellValid(i, j)) {
+                        map[i][j] = DOT_O;
+                        if (checkWin(DOT_O)) {
+                            x = i;
+                            y = j;
+                            ai_win = true;
+                        }
+                        map[i][j] = DOT_EMPTY;
+                    }
                 }
             }
         }
-    }
-
-    public static boolean isCellValid(int x, int y){
-        boolean result = true;
-
-        if(x < 0 || x >= SIZE || y < 0 || y >= SIZE) {
-            result = false;
-        }
-
-        if(map[y][x] != DOT_EMPTY){
-            result = false;
-        }
-        return result;
-    }
-
-    private static boolean isEndGame(char playerSymbol) {
-        boolean result = false;
-
-        printMap();
-
-        //проверяем необходимость следующего хода
-        if (checkWin(playerSymbol)) {
-            System.out.println("Победители " + playerSymbol);
-            result = true;
-        }
-
-        if (isMapFull()) {
-            System.out.println("Ничья");
-            result = true;
-        }
-
-        return result;
-    }
-
-    private static boolean isMapFull(){
-        boolean result = true;
-
-        for (int i = 0; i < SIZE; i++){
-            for (int j = 0; j < SIZE; j++){
-                if (map[i][j] == DOT_EMPTY)
-                    result = false;
+        // Блокировка хода пользователя, если он побеждает на следующем ходу
+        if (aiLevel == 2) {
+            if (!ai_win) {
+                for (int i = 0; i < SIZE; i++) {
+                    for (int j = 0; j < SIZE; j++) {
+                        if (!isCellValid(i, j)) {
+                            map[i][j] = DOT_X;
+                            if (checkWin(DOT_X)) {
+                                x = i;
+                                y = j;
+                                user_win = true;
+                            }
+                            map[i][j] = DOT_EMPTY;
+                        }
+                    }
+                }
             }
         }
 
-        return result;
+        if (!ai_win && !user_win) {
+            do {
+                x = random.nextInt(SIZE);
+                y = random.nextInt(SIZE);
+            }
+            while (isCellValid(x, y));
+        }
+        map[x][y] = DOT_O;
+        System.out.println("Компьютер выбрал ячейку " + (x + 1) + " " + (y + 1));
     }
 
-    private static boolean checkWin(char playerSymbol) {
+
+    public static boolean isCellValid(int x, int y) {
+        if (x < 0 || y < 0 || x > SIZE - 1 || y > SIZE - 1) {
+            return false;
+        }
+        return map[x][y] != DOT_EMPTY;
+    }
+
+    private static boolean checkWin(String playerSymbol) {
         boolean result = false;
 
         if(
